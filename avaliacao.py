412fa305c8a376ae0941d19beda4090ade72e1e0
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2025-02-04 18:03:08 -0300

    Create avaliacao.py

---
+++ avaliacao.py
@@ -0,0 +1,5 @@
+##Avaliação
+
+#Crie um programa que peça o nome, email, idade, senha e confirmação de senha. Ele deve possuir um botão de registrar que permita o usuário ser cadastrado.
+
+#Obs: O usuário só pode ser cadastrado se a senha e a confirmação da senha forem iguais.
